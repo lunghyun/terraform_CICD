122169306d477b7ea3d15a6795415d92110b8c78
lunghyun <nangmannsh@gmail.com>
2025-03-19 19:46:34 +0900

    특정 arn 설정

--- backend/backend.tf
+++ backend/backend.tf
@@ -29,27 +29,25 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
 
 resource "aws_s3_bucket_policy" "terraform_state_policy" {
   bucket = aws_s3_bucket.terraform_state.id
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": "*",
-      "Action": [
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Sid       = "AllowSpecificRole"
+      Effect    = "Allow"
+      Principal = { "AWS" : "arn:aws:iam::YOUR_AWS_ACCOUNT_ID:role/YOUR_ROLE_NAME" }
+      Action    = [
         "s3:GetBucketPolicy",
         "s3:ListBucket",
         "s3:GetObject",
         "s3:PutObject"
-      ],
-      "Resource": [
+      ]
+      Resource = [
         "arn:aws:s3:::mello-terraformstate",
         "arn:aws:s3:::mello-terraformstate/*"
       ]
-    }
-  ]
-}
-POLICY
+    }]
+  })
 }
 
 resource "aws_dynamodb_table" "terraform_lock" {
