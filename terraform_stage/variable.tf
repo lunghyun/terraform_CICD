fb7c9a2bb38fd9ea335dfe573c1795b46b293ae5
lunghyun <nangmannsh@gmail.com>
2025-03-25 15:28:28 +0900

    terraform_stage 디렉토리에서 모듈 관련된 variable 주석처리

--- terraform_stage/variable.tf
+++ terraform_stage/variable.tf
@@ -142,22 +142,22 @@ variable "my_ip" {
     default = "0.0.0.0/0"
 }
 
-variable "vpc_id" {
-  type = string
-}
+# variable "vpc_id" {
+#   type = string
+# }
 
-variable "subnet_service_az1_id" {
-  type = string
-}
+# variable "subnet_service_az1_id" {
+#   type = string
+# }
 
-variable "subnet_service_az2_id" {
-  type = string
-}
+# variable "subnet_service_az2_id" {
+#   type = string
+# }
 
-variable "subnet_service_az1_cidr" {
-  type = string
-}
+# variable "subnet_service_az1_cidr" {
+#   type = string
+# }
 
-variable "subnet_service_az2_cidr" {
-  type = string
-}
+# variable "subnet_service_az2_cidr" {
+#   type = string
+# }
