d4792bc510a8ea60b77e6a5ac9f51dc1fe85ee16
lunghyun <nangmannsh@gmail.com>
2025-03-26 13:26:36 +0900

    interval 늘리기

--- modules/alb/alb.tf
+++ modules/alb/alb.tf
@@ -130,13 +130,13 @@ resource "aws_lb_target_group" "target_asg" {
     vpc_id = var.vpc_id
 
     health_check {
-        path = "/"
-        protocol = "HTTPS"
-        matcher = "200"
-        interval = 15
-        timeout = 3
-        healthy_threshold = 2
-        unhealthy_threshold = 2
+        path                = "/"
+        protocol            = "HTTPS"
+        matcher             = "200"
+        interval            = 30
+        timeout             = 5
+        healthy_threshold   = 2
+        unhealthy_threshold = 5
     }
 
     lifecycle {
