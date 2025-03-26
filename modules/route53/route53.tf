5dff97c40fe0a3594878fea2b064b66145fc3853
lunghyun <nangmannsh@gmail.com>
2025-03-26 10:07:26 +0900

    route53 레코드 이름 변경

--- modules/route53/route53.tf
+++ modules/route53/route53.tf
@@ -6,7 +6,7 @@ data "aws_route53_zone" "primary" {
 
 resource "aws_route53_record" "primary" {
     zone_id = data.aws_route53_zone.primary.zone_id
-    name = var.alb_dns_name
+    name = "stage.hellomello.site"
     type = "A"
 
     alias {
