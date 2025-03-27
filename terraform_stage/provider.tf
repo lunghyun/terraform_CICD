721bb63b2e63d5fb2ea08c3d76ddac1a6479d390
lunghyun <nangmannsh@gmail.com>
2025-03-27 11:22:26 +0900

    acm 발급 과정 제거

--- terraform_stage/provider.tf
+++ terraform_stage/provider.tf
@@ -1,11 +1,3 @@
 provider "aws" {
   region = var.region # Please use the default region ID
 }
-
-# 버지니아 북부 - CloudFront용 인증서
-resource "aws_acm_certificate" "cloudfront_cert" {
-  provider          = aws.us_east
-  domain_name       = "hellomello.site"
-  subject_alternative_names = ["*.hellomello.site"]
-  validation_method = "DNS"
-}
